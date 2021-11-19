b2b5d7d35ab241c3f9f15fd74d869dd0600f44a3
Yoann Rodière <yoann@hibernate.org>
2021-11-19 09:46:15 +0100

    Move to JUnit 5

---
+++ src/test/java/org/hibernate/jdk/playground/MyTest.java
@@ -0,0 +1,13 @@
+package org.hibernate.jdk.playground;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+	@Test
+	void test() {
+		Assertions.fail();
+	}
+
+}
