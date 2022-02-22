9a2038daa4b4d8696657322e845d467a53c28ca9
Yoann Rodière <yoann@hibernate.org>
2022-02-22 10:55:44 +0100

    Test Annotation.toString()

--- src/test/java/org/hibernate/jdk/playground/MyTest.java
+++ src/test/java/org/hibernate/jdk/playground/MyTest.java
@@ -1,13 +1,33 @@
 package org.hibernate.jdk.playground;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.Test;
 
 public class MyTest {
 
 	@Test
-	void test() {
-		Assertions.fail();
+	void test() throws NoSuchFieldException {
+		Field myField = MyAnnotatedClass.class.getDeclaredField( "myField" );
+		MyAnnotation myAnnotation = myField.getAnnotation( MyAnnotation.class );
+		assertEquals( "@org.hibernate.jdk.playground.MyTest$MyAnnotation(myAttribute=\"foo\")", myAnnotation.toString() );
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.FIELD)
+	public @interface MyAnnotation {
+		String myAttribute();
+	}
+
+	public static class MyAnnotatedClass {
+		@MyAnnotation(myAttribute = "foo")
+		String myField;
 	}
 
 }
