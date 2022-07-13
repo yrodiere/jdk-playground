8466c7b97dc097d53629312a55e7eebecb8c1807
Yoann Rodière <yoann@hibernate.org>
2022-07-13 08:39:12 +0200

    Test isSynthetic/isImplicit

--- src/test/java/org/hibernate/jdk/playground/MyTest.java
+++ src/test/java/org/hibernate/jdk/playground/MyTest.java
@@ -1,13 +1,19 @@
 package org.hibernate.jdk.playground;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class MyTest {
 
 	@Test
-	void test() {
-		Assertions.fail();
+	void implicit() {
+		assertTrue( TestedClass.InnerClass.class.getConstructors()[0].getParameters()[0].isImplicit() );
+	}
+
+	@Test
+	void synthetic() {
+		assertTrue( TestedClass.InnerClass.class.getConstructors()[0].getParameters()[0].isSynthetic() );
 	}
 
 }
---
+++ src/test/java/org/hibernate/jdk/playground/TestedClass.java
@@ -0,0 +1,12 @@
+package org.hibernate.jdk.playground;
+
+public class TestedClass {
+
+	class InnerClass {
+		private int foo;
+
+		public InnerClass(int foo) {
+			this.foo = foo;
+		}
+	}
+}
