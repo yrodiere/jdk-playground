b06b615f2a039851e1a03d64d2ae146b6c3286da
Yoann Rodière <yoann@hibernate.org>
2024-06-25 15:34:48 +0200

    Add reproducer

--- src/test/java/org/hibernate/jdk/playground/MyTest.java
+++ src/test/java/org/hibernate/jdk/playground/MyTest.java
@@ -2,12 +2,35 @@ package org.hibernate.jdk.playground;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Testcontainers
 public class MyTest {
 
-	@Test
-	void test() {
-		Assertions.fail();
-	}
+    @Container
+    private static final PostgreSQLContainer POSTGRES = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withUsername("foo")
+            .withPassword("bar");
+
+    @Test
+    void test() {
+        try (
+                Connection conn = DriverManager.getConnection(POSTGRES.getJdbcUrl(), "foo", "bar");
+                Statement stmt = conn.createStatement();
+        ) {
+            stmt.execute("DROP TABLE IF EXISTS tableThatDoesNotexist");
+            Assertions.assertNull(stmt.getWarnings());
+        } catch (SQLException e) {
+            Assertions.fail("SQLException caught", e);
+        }
+    }
 
 }
